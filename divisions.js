3151ca71d704d3ec38ac1ca37a31147dd9f32867
AgentK0081 <harryjoji08@gmail.com>
2025-12-27 12:18:34 +0530

    Create divisions.js

---
+++ divisions.js
@@ -0,0 +1,14 @@
+const divisions = [
+  {
+    name: "Federal Bureau of Investigation",
+    description: "Primary federal law enforcement division.",
+    image: "https://i.imgur.com/4ZQZ4ZC.png",
+    discord: "https://discord.gg/YOURDISCORD"
+  },
+  {
+    name: "Hostage Rescue Team",
+    description: "High-risk tactical response unit.",
+    image: "https://i.imgur.com/qQ9XJ3k.png",
+    discord: "https://discord.gg/YOURDISCORD"
+  }
+];
